c3e35a5388688d96a34e1a1a87df2a1507487664
inkAja <mdghifari17@gmail.com>
2024-08-12 14:42:29 +0700

    fix

--- resources/js/Pages/Dashboard/Admin.vue
+++ resources/js/Pages/Dashboard/Admin.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
 
@@ -19,25 +18,3 @@ import AppLayout from '@/Layouts/AppLayout.vue';
         </div>
     </AppLayout>
 </template>
-=======
-<script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-
-</script>
-
-<template>
-    <AppLayout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Admin Panel
-            </h2>
-        </template>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                Admin Panel Content goes here.
-            </div>
-        </div>
-    </AppLayout>
-</template>
->>>>>>> f7d4836190d4a3a0cb6344051a14c0485303787d
