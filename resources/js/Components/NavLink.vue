4dd680b6ab8d0fd975206c593ee11d50626c97c3
fatee <fateehganteng@gmail.com>
2024-08-12 11:34:33 +0700

    navlink update

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -10,7 +10,7 @@ const props = defineProps({
 const classes = computed(() => {
     return props.active
         ? 'inline-flex items-center px-4 my-3 bg-secondary rounded-md border-secondary text-sm text-white font-medium leading-5 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-        : 'inline-flex items-center px-4 my-3 rounded-md border-transparent text-sm font-medium leading-5 text-white hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+        : 'inline-flex items-center px-4 my-3 rounded-md border-transparent text-sm font-medium leading-5 text-white hover:text-secondary hover:bg-white focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
 });
 </script>
 
