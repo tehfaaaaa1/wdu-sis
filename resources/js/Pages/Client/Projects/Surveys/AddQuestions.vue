c983318453997777d227d801b62f1bff125f4594
fatee <fateehganteng@gmail.com>
2024-08-22 14:06:39 +0700

    Add Qs

--- resources/js/Pages/Client/Projects/Surveys/AddQuestions.vue
+++ resources/js/Pages/Client/Projects/Surveys/AddQuestions.vue
@@ -15,40 +15,25 @@ import DropdownLink from '@/Components/DropdownLink.vue';
 
 const props = defineProps({ surveys: Object, projects: Object })
 
-const questions = ref([])
-const choices = ref([])
+const form = useForm({})
+
+const questions = ref([{ id: 1, soal: '', texts: [], radios: [] }])
+const radios = ref([])
 const texts = ref([])
 
-const form = useForm({
-    title: '',
-    desc: '',
-});
+function addQuestion() {
+    questions.value.push({ id: questions.value.length + 1, soal: '', texts: [], radios: [] });
+}
 
-let idquestion =1
-function addQuestion(){
-    const question={
-        id: idquestion++,
-        soal: '',
-    }
-    questions.value.push(question)
+function textQuestion(question) {
+    const text = { isi: '' };
+    question.texts.push(text);
 }
-let idchoice =1
-function radioQuestion(){
-    const choice={
-        id: idchoice++,
-        pilih: '',
-    }
-    choices.value.push(choice)
+
+function radioQuestion(question) {
+    const radio = { pilih: '' };
+    question.radios.push(radio);
 }
-let idtext = 1  
-function textQuestion(){
-    const text={
-        id: idtext++,
-        isi: '',
-    }
-    texts.value.push(text)
-}
-console.log(props)
 
 </script>
 
@@ -66,85 +51,69 @@ console.log(props)
                             {{ props.surveys.desc }}
                         </p>
                     </div>
-                    <form action="" method="post" @submit.prevent >
-                        <div id="questions" v-for="question in questions" > <!-- v-for here -->
-                            <div>
-                                <div class="p-5 flex items-center">
-                                    <!-- Order of question -->
-                                    <p>{{ question.id }}</p>
-    
-                                    <!-- Insert text here -->
-                                    <input v-model="question.soal" type="text" placeholder="Insert question here"
-                                        class="text-sm w-full mx-4 rounded-md">
+                    <form action="" method="post" @submit.prevent>
+                        <div v-for="question in questions" :id="'question-' + question.id" class="mb-4">
+                            <!-- v-for here -->
+                            <div class="p-5 flex items-center">
+                                <!-- Order of question -->
+                                <p>{{ question.id }}</p>
 
-                                    <!-- Question types -->
-                                    <Dropdown align="right" width="48">
-                                        <template #trigger>
-                                            <button type="button"
-                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                                Question Type
-                                                <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                                    fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                                    stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                                </svg>
-                                            </button>
-                                        </template>
+                                <!-- Insert text here -->
+                                <input v-model="question.soal" type="text" placeholder="Insert question here"
+                                    class="text-sm w-full mx-4 rounded-md">
 
-                                        <template #content>
-                                            <div @click="textQuestion" class="block px-4 py-2 text-sm cursor-pointer">
-                                                Text
-                                            </div>
-                                            <div @click="radioQuestion" class="block px-4 py-2 text-sm cursor-pointer">
-                                                Single Choice
-                                            </div>
-                                            <div class="block px-4 py-2 text-sm cursor-pointer">
-                                                Multiple Choice
-                                            </div>
-                                        </template>
-                                    </Dropdown>
+                                <!-- Question types -->
+                                <Dropdown align="right" width="48">
+                                    <template #trigger>
+                                        <button type="button"
+                                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                            Question Type
+                                            <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                                stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                            </svg>
+                                        </button>
+                                    </template>
 
-                                </div>
-                                <!-- single choice -->
-                                <div class="p-5" v-for="choice in choices">
-                                    <input type="text" v-model="choice.pilih1" :name="idchoice" id="q1"
-                                        placeholder="Insert single choice here" class="text-sm mx-4 mb-2 rounded-md block w-1/4">
-                                    <input type="text" v-model="choice.pilih2" :name="idchoice" id="q2"
-                                        placeholder="Insert single choice here" class="text-sm mx-4 mb-2 rounded-md block w-1/4">
-                                    <input type="text" v-model="choice.pilih3" :name="idchoice" id="q3"
-                                        placeholder="Insert single choice here" class="text-sm mx-4 mb-2 rounded-md block w-1/4">
-                                </div>
-                                <!-- text -->
-                                <div class="p-5" v-for="text in texts">
-                                    <textarea v-model="text.isi" :name="'text-' + idtext" :id="'text-' + idtext"
-                                        placeholder="Jawaban" class="w-full text-sm rounded-md bg-gray-200" disabled />
-                                </div>
+                                    <template #content>
+                                        <div @click="textQuestion(question)"
+                                            class="block px-4 py-2 text-sm cursor-pointer">
+                                            Text
+                                        </div>
+                                        <div @click="radioQuestion(question)"
+                                            class="block px-4 py-2 text-sm cursor-pointer">
+                                            Single Choice
+                                        </div>
+                                        <div class="block px-4 py-2 text-sm cursor-pointer">
+                                            Multiple Choice
+                                        </div>
+                                    </template>
+                                </Dropdown>
+                            </div>
+
+                            <!-- text -->
+                            <div class="px-5" v-for="(text, index) in question.texts" :key="index">
+                                <textarea v-model="text.isi" :name="'text-' + question.id" :id="'text-' + question.id"
+                                    placeholder="Jawaban" class="w-full text-sm rounded-md bg-gray-200" disabled />
                             </div>
                             <!-- single choice -->
-                            <div class="" v-for="choice in choices">
-                                     <div class="p-5">
-                                         <p>{{ choice.id }}</p>
-                                         <input type="text" v-model="choice.pilih" :name="idchoice" :id="idchoice" placeholder="Insert single choice here" class="text-sm mx-4 rounded-md">
-                                     </div>
-                                 </div>
-                                 <!-- text -->
-                                    <div class="" v-for="text in texts">
-                                        <div class="">
-                                            <div class="p-5" >
-                                                <p>{{ text.id }}</p>
-                                                <textarea v-model="text.isi" name="1" id="q1" placeholder="jawaban"
-                                                class="text-sm mx-4 rounded-md"/>
-                                            </div>
-                                        </div>
-                                    </div>
+                            <div class="px-5" v-for="(radio, index) in question.radios" :key="index">
+                                <div class="flex items-center mb-2">
+                                    &#x2022;
+                                    <input type="text" v-model="radio.pilih" :name="'radio-' + question.id"
+                                        :id="'radio-' + question.id" placeholder="Insert single choice here"
+                                        class="text-sm mx-4 rounded-md block w-1/4">
+                                </div>
+                            </div>
                         </div>
                         <!-- End question -->
 
                         <div class="pt-2 flex justify-center">
                             <button
                                 class="w-1/4 mb-10 flex justify-center py-2.5 my-0 text-white !bg-primary rounded-md text-sm hover:!bg-transparent hover:text-primary hover:outline hover:outline-primary transition hover:duration-200"
-                                 @click="addQuestion">
+                                @click="addQuestion">
                                 Add Questions
                             </button>
                         </div>
