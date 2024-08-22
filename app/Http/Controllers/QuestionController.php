62683d1b912c1df2f5e2e97d0652d645542a22cb
inkAja <mdghifari17@gmail.com>
2024-08-22 10:32:04 +0700

    question type

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Survey;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Inertia\Inertia;
 
 class QuestionController extends Controller
 {
-    //
+    public function question(Survey $survey, $clientSlug, $projectSlug, $id)
+    {
+        $survey =  Survey::findOrFail($id);
+
+        $project = DB::table('projects')
+            ->where('slug', $projectSlug)
+            ->get();
+        $client = DB::table('clients')
+            ->where('slug', $clientSlug)
+            ->get();
+        // dump($survey);
+        return Inertia::render(
+            'Client/Projects/Surveys/AddQuestions',
+            [
+                'surveys' => $survey,
+                'projects' => $project,
+                'clients' => $client,
+            ]
+        );
+    }
 }
--- resources/js/Pages/Client/Projects/Surveys/AddQuestions.vue
+++ resources/js/Pages/Client/Projects/Surveys/AddQuestions.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Head, Link, useForm } from '@inertiajs/vue3';
+import { ref, computed } from 'vue';
 import AuthenticationCard from '@/Components/AuthenticationCard.vue';
 import AuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
 import Checkbox from '@/Components/Checkbox.vue';
@@ -13,6 +14,9 @@ import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 
 const props = defineProps({ surveys: Object, projects: Object })
+const choices = ref([])
+
+const texts = ref([])
 
 const form = useForm({
     title: '',
@@ -21,7 +25,23 @@ const form = useForm({
 const submit = () => {
     // form.put(route('update_survey', props.surveys.id));
 };
+let idchoice =1
 
+function radioQuestion(){
+    const choice={
+        id: idchoice++,
+        pilih: '',
+    }
+    choices.value.push(choice)
+}
+let idtext = 1
+function textQuestion(){
+    const text={
+        id: idtext++,
+        isi: '',
+    }
+    texts.value.push(text)
+}
 console.log(props)
 
 </script>
@@ -42,42 +62,56 @@ console.log(props)
                     </div>
                     <form action="" method="post" @submit.prevent>
                         <div id="questions"> <!-- v-for here -->
-                            <div class="p-5 flex items-center">
-                                <!-- Order of question -->
-                                <p>1. </p>
-
-                                <!-- Insert text here -->
-                                <input type="text" name="1" id="q1" placeholder="Insert question here"
-                                    class="text-sm w-full mx-4 rounded-md">
-
-                                <!-- Question types -->
-                                <Dropdown align="right" width="48">
-                                    <template #trigger>
-                                        <button type="button"
-                                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                            Question Type
-
-                                            <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                                stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                            </svg>
-                                        </button>
-                                    </template>
-
-                                    <template #content>
-                                        <div class="block px-4 py-2 text-sm cursor-pointer">
-                                            Text
-                                        </div>
-                                        <div class="block px-4 py-2 text-sm cursor-pointer">
-                                            Single Choice
-                                        </div>
-                                        <div class="block px-4 py-2 text-sm cursor-pointer">
-                                            Multiple Choice
-                                        </div>
-                                    </template>
-                                </Dropdown>
+                            <div>
+                                <div class="p-5 flex items-center">
+                                    <!-- Order of question -->
+                                    <p>1. </p>
+    
+                                    <!-- Insert text here -->
+                                    <input type="text" name="1" id="q1" placeholder="Insert question here"
+                                        class="text-sm w-full mx-4 rounded-md">
+    
+                                    <!-- Question types -->
+                                    <Dropdown align="right" width="48">
+                                        <template #trigger>
+                                            <button type="button"
+                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                                Question Type
+    
+                                                <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                                    stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                                </svg>
+                                            </button>
+                                        </template>
+    
+                                        <template #content>
+                                            <div @click="textQuestion" class="block px-4 py-2 text-sm cursor-pointer">
+                                                Text
+                                            </div>
+                                            <div @click="radioQuestion" class="block px-4 py-2 text-sm cursor-pointer">
+                                                Single Choice
+                                            </div>
+                                            <div class="block px-4 py-2 text-sm cursor-pointer">
+                                                Multiple Choice
+                                            </div>
+                                        </template>
+                                    </Dropdown>
+                                    
+                                </div>
+                                <!-- single choice -->
+                                <div class="p-5" v-for="choice in choices">
+                                    <p>1</p>
+                                    <input type="text" v-model="choice.pilih" name="1" id="q1" placeholder="Insert single choice here"
+                                        class="text-sm mx-4 rounded-md">
+                                </div>
+                                <div class="p-5" v-for="text in texts">
+                                    <p>1</p>
+                                    <textarea v-model="text.isi" name="1" id="q1" placeholder="jawaban"
+                                        class="text-sm mx-4 rounded-md"/>
+                                </div>
                             </div>
                         </div>
                         <div class="pt-2 flex justify-center">
