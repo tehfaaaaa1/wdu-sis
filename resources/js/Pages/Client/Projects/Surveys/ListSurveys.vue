a1caf739a18801be7647420a68e5494cce07abc2
fatee <fateehganteng@gmail.com>
2024-09-09 14:45:09 +0700

    fix

--- resources/js/Pages/Client/Projects/Surveys/ListSurveys.vue
+++ resources/js/Pages/Client/Projects/Surveys/ListSurveys.vue
@@ -140,9 +140,13 @@ const getSurveySubmissions = (surveyId) => {
                                         {{ getProvinceName(survey.province_id) }}
                                     </span>
                                 </td>
+                                <td class="px-6 py-4">
+                                    <p v-if="survey.status == 0"> Ditutup</p>
+                                    <p v-if="survey.status == 1"> Dibuka</p>
+                                </td>
                                 <td class="px-6 py-4">Dibuka</td>
                                 <td class="px-6 py-6 grid grid-cols-2 gap-x-2 justify-center">
-                                    <div v-if="hasFilledSurvey(survey)">
+                                    <div v-if="hasFilledSurvey(survey)" class="my-3">
                                         <p class="text-center">Anda Sudah Mengisi Survey Ini</p>
                                     </div>
                                     <NavLink v-if="!hasFilledSurvey(survey)"
@@ -166,7 +170,7 @@ const getSurveySubmissions = (surveyId) => {
                                         class="mt-5 text-center col-span-2">
                                         <a :href="route('edit_surveys', [clientSlug, projectSlug, survey.id])"
                                             class="font-medium text-blue-600 hover:underline mr-4">Edit</a>
-                                        <a @click="hapus(clientSlug, projectSlug, survey.id)" class="font-medium text-red-600 hover:underline">Delete</a>
+                                        <a @click="hapus(clientSlug, projectSlug, survey.id)" class="font-medium text-red-600 hover:underline cursor-pointer">Delete</a>
                                     </div>
                                 </td>
                             </tr>
