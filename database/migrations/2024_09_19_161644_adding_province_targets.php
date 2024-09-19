1905e163b900c4142e2c07bb5312d15666fc72ee
inkAja <mdghifari17@gmail.com>
2024-09-19 16:23:24 +0700

    err fix migration

--- database/migrations/2024_09_19_161644_adding_province_targets.php
+++ database/migrations/2024_09_19_161644_adding_province_targets.php
@@ -10,7 +10,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('surveys', function (Blueprint $table) {
-            $table->json('province_targets')->change();
+            $table->json('province_targets');
         });
     }
 
