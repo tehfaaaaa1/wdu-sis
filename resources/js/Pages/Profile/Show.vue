f15aba3bf6702f3564a0f383b129463fad714b89
Fadel <fadel.yulis123@gmail.com>
2024-09-23 15:16:30 +0700

    tes

--- resources/js/Pages/Profile/Show.vue
+++ resources/js/Pages/Profile/Show.vue
@@ -17,7 +17,9 @@ defineProps({
     <AppLayout title="Profile">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 Profile
+                </div>   
             </h2>
         </template>
 
