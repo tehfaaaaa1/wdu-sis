418cb88be3f01625c4079388628a3abd3586c0f5
fatee <fateehganteng@gmail.com>
2024-09-25 10:32:38 +0700

    tidying allreport

--- resources/js/Pages/Client/Projects/Surveys/Report/AllReport.vue
+++ resources/js/Pages/Client/Projects/Surveys/Report/AllReport.vue
@@ -17,9 +17,9 @@ const props = defineProps({
 const project = props.projects[0]
 const client = props.clients[0]
 
-const PieChartData =(
-    props.page.map((page)=>({
-        question:page.question.map(()=>({
+const PieChartData = (
+    props.page.map((page) => ({
+        question: page.question.map(() => ({
             labels: [],
             datasets: [{
                 label: ['Response'],
@@ -30,52 +30,51 @@ const PieChartData =(
     }))
 )
 
-const chartOptions = {
-  responsive: true,
-  plugins: {
-    legend: {
-      display: true,
-      position: 'bottom',   
-    },
-    title: {
-      display: true,
-      text: 'Data',
-    },
-  },
-};
-
-
 const barBackgroundColors = ['#41B883', '#E46651', '#00D8FF', '#DD1B16']
-const BarChartData =(
-    props.page.map((page)=>({
-        question:page.question.map((q)=>({
+const BarChartData = (
+    props.page.map((page) => ({
+        question: page.question.map((q) => ({
             labels: ['Response'],
-            datasets: q.choice.map((c, ini)=>({
-                label: c.value +' ('+((q.answer.filter(a=>a.answer == c.id).length *100) / q.answer.length).toFixed(2)+'%)',
-                backgroundColor: barBackgroundColors[ini],
-                data: [q.answer.filter(a=>a.answer == c.id).length]
+            datasets: q.choice.map((c, index) => ({
+                label: c.value + ' (' + ((q.answer.filter(a => a.answer == c.id).length * 100) / q.answer.length).toFixed(2) + '%)',
+                backgroundColor: barBackgroundColors[index],
+                data: [q.answer.filter(a => a.answer == c.id).length]
             }))
         }))
     }))
 )
-console.log(BarChartData)
+
+const chartOptions = {
+    responsive: true,
+    plugins: {
+        legend: {
+            display: true,
+            position: 'bottom',
+        },
+        title: {
+            display: true,
+            text: 'Data Kuisioner',
+        },
+    },
+};
+
 const count = (pgind, qind, choice, answer, question) => {
     const all = answer.length
 
-    if(PieChartData[pgind].question[qind].labels.length != choice.length){
+    if (PieChartData[pgind].question[qind].labels.length != choice.length) {
         choice.forEach((element, ind) => {
-            const persentage = ((answer.filter(a=>a.answer == element.id).length *100) / all).toFixed(2)
-            if(question.question_type_id == 2){
-                PieChartData[pgind].question[qind].labels.push(element.value+' ('+ persentage +'%)' )
-                PieChartData[pgind].question[qind].datasets[0].data.push(answer.filter(a=>a.answer == element.id).length)
-            } 
+            const persentage = ((answer.filter(a => a.answer == element.id).length * 100) / all).toFixed(2)
+            if (question.question_type_id == 2) {
+                PieChartData[pgind].question[qind].labels.push(element.value + ' (' + persentage + '%)')
+                PieChartData[pgind].question[qind].datasets[0].data.push(answer.filter(a => a.answer == element.id).length)
+            }
         })
     } else {
         return false
     }
-}    
-const showAllanswer = ref(props.page.map((p)=>({
-    q: p.question.map(()=>({
+}
+const showAllanswer = ref(props.page.map((p) => ({
+    q: p.question.map(() => ({
         value: false
     }))
 })))
@@ -97,19 +96,22 @@ const showAllanswer = ref(props.page.map((p)=>({
                                     :key="index">
                                     <div class="block mb-2.5">
                                         <p class="font-semibold">
-                                            {{ index + 1 }}. <label>{{question.question_text }}</label>
+                                            {{ index + 1 }}. <label>{{ question.question_text }}</label>
                                         </p>
 
                                         <!-- Handling radio inputs for question type 2 -->
                                         <div v-if="question.question_type_id == 2" class="flex gap-x-10">
                                             <div class="">
                                                 <div v-for="(list, i) in question.choice" :key="i">
-                                                    <input type="radio" :name="'radio' + list.id" :id="'radio' + list.id"
-                                                        disabled>
-                                                    <label :for="'radio' + list.id" class="px-3">{{ list.value }}</label>
+                                                    <input type="radio" :name="'radio' + list.id"
+                                                        :id="'radio' + list.id" disabled>
+                                                    <label :for="'radio' + list.id" class="px-3">{{ list.value
+                                                        }}</label>
                                                 </div>
                                             </div>
-                                            <PieChart :chart-data="PieChartData[ind].question[index]" :key="count(ind, index, question.choice, question.answer, question)" :chart-options="chartOptions" />
+                                            <PieChart :chart-data="PieChartData[ind].question[index]"
+                                                :key="count(ind, index, question.choice, question.answer, question)"
+                                                :chart-options="chartOptions" />
                                         </div>
 
                                         <!-- Handle checkbox for question type 3 -->
@@ -118,25 +120,37 @@ const showAllanswer = ref(props.page.map((p)=>({
                                                 <div class="" v-for="(list, i) in question.choice" :key="i">
                                                     <input type="checkbox" :name="'checkbox' + list.id"
                                                         :id="'checkbox' + list.id" disabled>
-                                                    <label :for="'checkbox' + list.id" class="px-3">{{ list.value }}</label>
+                                                    <label :for="'checkbox' + list.id" class="px-3">{{ list.value
+                                                        }}</label>
                                                 </div>
                                             </div>
-                                            <BarChart :chart-data="BarChartData[ind].question[index]" :key="count(ind, index, question.choice, question.answer, question)" :chart-options="chartOptions" />
+                                            <BarChart :chart-data="BarChartData[ind].question[index]"
+                                                :key="count(ind, index, question.choice, question.answer, question)"
+                                                :chart-options="chartOptions" />
                                         </div>
 
                                         <!-- Handling textarea for question type 1 -->
                                         <div v-if="question.question_type_id == 1">
-                                            <div class="px-5 mt-2 " v-for="(answe , Aindex) in question.answer" :key="Aindex">
-                                                <div class="" v-if="Aindex < 3 || showAllanswer[ind].q[index].value == true">
+                                            <div class="px-5 mt-2 " v-for="(answe, Aindex) in question.answer"
+                                                :key="Aindex">
+                                                <div class=""
+                                                    v-if="Aindex < 3 || showAllanswer[ind].q[index].value == true">
                                                     <input v-if="answe.question_id == question.id" type="text"
-                                                    :value="answe.answer" disabled class="rounded w-full">
+                                                        :value="answe.answer" disabled class="rounded w-full">
                                                 </div>
                                             </div>
-                                            <div class="flex justify-center mt-2" v-if="showAllanswer[ind].q[index].value == false">
-                                                <button class="text-sm text-secondary border-b border-transparent hover:border-secondary transition p-1 focus:border-secondary focus:outline-none" type="button" @click="showAllanswer[ind].q[index].value = true">Show All</button>
+                                            <div class="flex justify-center mt-2"
+                                                v-if="showAllanswer[ind].q[index].value == false">
+                                                <button
+                                                    class="text-sm text-secondary border-b border-transparent hover:border-secondary transition p-1 focus:border-secondary focus:outline-none"
+                                                    type="button" @click="showAllanswer[ind].q[index].value = true">Show
+                                                    All</button>
                                             </div>
                                             <div class="flex justify-center mt-2" v-else>
-                                                <button type="button" class="text-sm text-secondary border-b border-transparent hover:border-secondary transition p-1 focus:border-secondary focus:outline-none"  @click="showAllanswer[ind].q[index].value = false">Show Less</button>
+                                                <button type="button"
+                                                    class="text-sm text-secondary border-b border-transparent hover:border-secondary transition p-1 focus:border-secondary focus:outline-none"
+                                                    @click="showAllanswer[ind].q[index].value = false">Show
+                                                    Less</button>
                                             </div>
                                         </div>
                                     </div>
