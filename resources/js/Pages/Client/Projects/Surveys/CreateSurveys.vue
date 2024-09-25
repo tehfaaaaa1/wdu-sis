f1e9bef8894f4e79dd80dfecbe38e82c77b65b21
Fadel <fadel.yulis123@gmail.com>
2024-09-25 09:53:20 +0700

    fixing UI createsurvey regencies and cities

--- resources/js/Pages/Client/Projects/Surveys/CreateSurveys.vue
+++ resources/js/Pages/Client/Projects/Surveys/CreateSurveys.vue
@@ -62,54 +62,72 @@ const toggleProvinceTarget = (provinceId) => {
             province_id: provinceId,
             target_response: '',
             province_name: selected.name,
-            cities: [], 
-            regencies: [] 
+            cities: [],
+            regencies: []
         });
         selectedProvince.value = provinceId;
-        fetchCitiesAndRegencies(provinceId); 
+        fetchCitiesAndRegencies(provinceId);
     } else {
         form.province_targets.splice(index, 1);
         selectedProvince.value = null;
-        cities.value = [];
-        regencies.value = [];
     }
 };
-
 
 const toggleCity = (provinceId, cityId) => {
     const provinceIndex = form.province_targets.findIndex(p => p.province_id === provinceId);
-    const cityIndex = form.province_targets[provinceIndex].cities.indexOf(cityId);
 
-    if (cityIndex === -1) {
-        form.province_targets[provinceIndex].cities.push(cityId);
-    } else {
-        form.province_targets[provinceIndex].cities.splice(cityIndex, 1);
+    if (provinceIndex !== -1) {
+        const cityIndex = form.province_targets[provinceIndex].cities.findIndex(c => c.city_id === cityId);
+        if (cityIndex === -1) {
+            form.province_targets[provinceIndex].cities.push({
+                city_id: cityId,
+                target_response_city: '',
+                showCityResponse: true, 
+            });
+        } else {
+            form.province_targets[provinceIndex].cities.splice(cityIndex, 1);
+        }
     }
 };
-
 
 const toggleRegency = (provinceId, regencyId) => {
     const provinceIndex = form.province_targets.findIndex(p => p.province_id === provinceId);
-    const regencyIndex = form.province_targets[provinceIndex].regencies.indexOf(regencyId);
 
-    if (regencyIndex === -1) {
-        form.province_targets[provinceIndex].regencies.push(regencyId);
-    } else {
-        form.province_targets[provinceIndex].regencies.splice(regencyIndex, 1);
+    if (provinceIndex !== -1) {
+        const regencyIndex = form.province_targets[provinceIndex].regencies.findIndex(c => c.regency_id === regencyId);
+        if (regencyIndex === -1) {
+            form.province_targets[provinceIndex].regencies.push({
+                regency_id: regencyId,
+                target_response_regency: '',
+                showRegencyResponse: true,
+            });
+        } else {
+            form.province_targets[provinceIndex].regencies.splice(regencyIndex, 1);
+        }
     }
 };
 
-
 const submit = () => {
+    const filteredProvinceTargets = form.province_targets.map(provinceTarget => ({
+        province_id: provinceTarget.province_id,
+        target_response: provinceTarget.target_response,
+        cities: provinceTarget.cities, 
+        regencies: provinceTarget.regencies, 
+        province_name: provinceTarget.province_name,
+        target_response_city: provinceTarget.target_response_city,
+        target_response_regency: provinceTarget.target_response_regency
+    }));
+
     form.post(route('create_survey', [form.client_slug, form.project_slug]), {
         data: {
             title: form.title,
             desc: form.desc,
             project_id: form.project_id,
-            province_targets: form.province_targets,
+            province_targets: filteredProvinceTargets,
         },
     });
 };
+
 </script>
 
 <template>
@@ -136,41 +154,55 @@ const submit = () => {
 
                     <div class="mt-4 relative">
                         <h3 class="text-center">Provinces:</h3>
-                        <div class="grid grid-rows-13 grid-flow-col text-xs">
-                            <div v-for="province in provinces" :key="province.id" class="flex items-center mb-2">
+                        <div class="grid grid-cols-3 mt-1 text-xs gap-2">
+                            <div v-for="province in provinces" :key="province.id" class="flex items-center gap-0.5">
                                 <input class="checked:text-primary checked:ring-primary focus:ring-primary" type="checkbox" :id="`province-${province.id}`" @change="toggleProvinceTarget(province.id)" />
                                 <label :for="`province-${province.id}`" class="ml-2">{{province.name}}</label>
                             </div>
                         </div>
                     </div>
 
-                    <!-- Cities, Regencies, and Target Response for selected provinces -->
                     <div v-for="(provinceTarget, index) in form.province_targets" :key="index" class="mt-4 relative">
                         <h4 class="text-primary font-semibold">{{ provinceTarget.province_name }}</h4>
 
                         <h5 class="mt-2 text-primary">Target Response:</h5>
                         <TextInput v-model="provinceTarget.target_response" type="number" placeholder="Enter target response" class="w-full" />
-                        <InputError class="mt-2" :message="form.errors[`province_targets.${index}.target_response`]" />
 
-                        <h5 class="mt-2 text-primary">Select Cities:</h5>
-                        <div v-for="city in cities" :key="city.id" class="flex items-center mb-2">
-                            <input type="checkbox" :id="`city-${city.id}`" @change="toggleCity(provinceTarget.province_id, city.id)" />
-                            <label :for="`city-${city.id}`" class="ml-2">{{ city.name }}</label>
+                        <h3 class="text-center mt-5">Cities:</h3>
+                        <div class="grid grid-cols-3 mt-1">
+                            <div v-for="city in cities" :key="city.id" class="flex items-center mb-2">
+                                <div class="text-xs">
+                                    <input type="checkbox" :id="`city-${city.id}`" @change="toggleCity(provinceTarget.province_id, city.id)" />
+                                    <label :for="`city-${city.id}`" class="ml-2">{{ city.name }}</label>
+                                </div>
+                            </div>
                         </div>
 
-                        <h5 class="mt-2 text-primary">Target Response City:</h5>
-                        <TextInput v-model="provinceTarget.target_response_city" type="number" placeholder="Enter city's target response" class="w-full" />
-                        <InputError class="mt-2" :message="form.errors[`province_targets.${index}.target_response_city`]" />
 
-                        <h5 class="mt-4 text-primary">Select Regencies:</h5>
-                        <div v-for="regency in regencies" :key="regency.id" class="flex items-center mb-2">
-                            <input type="checkbox" :id="`regency-${regency.id}`" @change="toggleRegency(provinceTarget.province_id, regency.id)" />
-                            <label :for="`regency-${regency.id}`" class="ml-2">{{ regency.name }}</label>
+                        <div v-for="cityTarget in provinceTarget.cities" :key="cityTarget.city_id">
+                            <div v-if="cityTarget.showCityResponse">
+                                <h5 class="mt-2 text-primary">Target Response City:</h5>
+                                <TextInput v-model="cityTarget.target_response_city" type="number" placeholder="Enter city's target response" class="w-full" />
+                            </div>
                         </div>
 
-                        <h5 class="mt-2 text-primary">Target Response Regency:</h5>
-                        <TextInput v-model="provinceTarget.target_response_regency" type="number" placeholder="Enter regency's target response" class="w-full" />
-                        <InputError class="mt-2" :message="form.errors[`province_targets.${index}.target_response_regency`]" />
+                        <h3 class="text-center mt-5">Regencies:</h3>
+                        <div class="grid grid-cols-3 mt-1">
+                            <div v-for="regency in regencies" :key="regency.id" class="flex items-center mb-2">
+                                <div class="text-xs">
+                                    <input type="checkbox" :id="`regency-${regency.id}`" @change="toggleRegency(provinceTarget.province_id, regency.id)" />
+                                    <label :for="`regency-${regency.id}`" class="ml-2">{{ regency.name }}</label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div v-for="regencyTarget in provinceTarget.regencies" :key="regencyTarget.regency_id">
+                            <div v-if="regencyTarget.showRegencyResponse">
+                                <h5 class="mt-2 text-primary">Target Response Regency:</h5>
+                                <TextInput v-model="provinceTarget.target_response_regency" type="number" placeholder="Enter regency's target response" class="w-full" />
+                                <InputError class="mt-2" :message="form.errors[`province_targets.${index}.target_response_regency`]" />
+                            </div>
+                        </div>
                     </div>
 
                     <div class="my-4 text-center">
