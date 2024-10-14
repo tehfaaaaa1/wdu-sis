fec9a5d68b576d479c40f7f9557f57db79d488f6
fatee <fateehganteng@gmail.com>
2024-10-14 10:53:44 +0700

    migration rollback

--- database/migrations/2024_10_11_161754_create_contact_recipients_table.php
+++ database/migrations/2024_10_11_161754_create_contact_recipients_table.php
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('campaigns');
+        Schema::dropIfExists('contact_recipients');
     }
 };
